aa5381d9a5fcb163fabcf34cf9ea4fca1063aecd
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2023-11-29 14:53:31 -0500

    Update views.py

--- marksheet_app/views.py
+++ marksheet_app/views.py
@@ -1,1 +1,18 @@
+# marksheet_app/views.py
+from django.shortcuts import render, redirect
+from .models import Student
+from .forms import StudentForm
 
+def marksheet(request):
+    students = Student.objects.all()
+    return render(request, 'marksheet_app/marksheet.html', {'students': students})
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('marksheet')
+    else:
+        form = StudentForm()
+    return render(request, 'marksheet_app/add_student.html', {'form': form})
